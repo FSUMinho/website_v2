a367540ccac52e2aa12a7d4c8df4ccee5416ac67
Duarte Leitão <duarte0903@gmail.com>
2024-07-04 19:26:20 +0100

    error page

--- fsuminho_website/src/pages/no_page.jsx
+++ fsuminho_website/src/pages/no_page.jsx
@@ -1,4 +1,10 @@
+import React, { useEffect } from 'react'
+
 const No_Page = () => {
+    useEffect(() => {
+        document.title = "Error"
+    }, [])
+    
     return (
         <div>
             <p>Ups something went wrong</p>
