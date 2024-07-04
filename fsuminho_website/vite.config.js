35d9f3f084ff5768e8fb64b7db6808795129a593
Duarte Leitão <duarte0903@gmail.com>
2024-07-04 19:32:40 +0100

    vite config

--- fsuminho_website/vite.config.js
+++ fsuminho_website/vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/fsuminho_website"
+  base: "/fsuminho_website/"
 })
