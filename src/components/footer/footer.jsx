6b60956bbfdc3390b3371711cd1e2f4592d8b2ad
Duarte Leitão <duarte0903@gmail.com>
2024-07-16 23:58:33 +0100

    fs classes improved + footer links

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -3,6 +3,7 @@ import './footer.css';
 import instagram from '../../assets/instagram.png'
 import linkedin from '../../assets/linkedin.png'
 import { useTranslation } from 'react-i18next';
+import { Link } from 'react-router-dom';
 
 const Footer = () => {
     const { t } = useTranslation();
@@ -18,25 +19,25 @@ const Footer = () => {
             </div>
 
             <div className="footer-section">
-            <h4>{t('footer.social')}</h4>
-            <div className="social-icons">
-                <a href="https://www.instagram.com/fsuminho/">
-                    <img src={instagram} className='icon' />
-                </a>
-                
-                <a href="https://www.linkedin.com/company/fsuminho">
-                    <img src={linkedin} className='icon' />
-                </a>
-            </div>
+                <h4>{t('footer.social')}</h4>
+                <div className="social-icons">
+                    <a href="https://www.instagram.com/fsuminho/">
+                        <img src={instagram} className='icon' />
+                    </a>
+                    
+                    <a href="https://www.linkedin.com/company/fsuminho">
+                        <img src={linkedin} className='icon' />
+                    </a>
+                </div>
             </div>
 
             <div className="footer-section">
             <h4>Links</h4>
             <ul>
-                <li><a href="/about">About Us</a></li>
-                <li><a href="/services">Services</a></li>
-                <li><a href="/contact">Contact</a></li>
-                <li><a href="/privacy">Privacy Policy</a></li>
+                <li><Link to="/team">{t('footer.team')}</Link></li>
+                <li><Link to="/sponsors">{t('footer.sponsors')}</Link></li>
+                <li><Link to="/invest">{t('footer.invest')}</Link></li>
+                <li><Link to="/archive">{t('footer.archive')}</Link></li>
             </ul>
             </div>
         </div>
