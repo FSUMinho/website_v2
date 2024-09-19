c2be7ae6fe4f2b73df77ce082e85db0a5b6f47b7
Duarte Leitão <duarte0903@gmail.com>
2024-09-19 18:30:07 +0100

    archive update

--- src/pages/team_archive/competitons/competitions.jsx
+++ src/pages/team_archive/competitons/competitions.jsx
@@ -4,6 +4,7 @@ import { useTranslation } from 'react-i18next';
 import Competition from '../../../components/competition/competition';
 import FSPT_2023_group_photo from '../../../assets/fspt_group_photo.jpg';
 import FSPT_logo from '../../../assets/archive_assets/FSPT_Logo_White_Square.svg';
+import fspt24 from '../../../assets/archive_assets/fspt24.jpg';
 
 const Archive = () => {
     const { t } = useTranslation();
@@ -26,6 +27,22 @@ const Archive = () => {
                         "Business Plan Presentation": "1", 
                         "Engineering Design": "2", 
                         "Cost of Manufacturing": "5"
+                    }}
+                />
+
+                <Competition
+                    background={fspt24}
+                    logo={FSPT_logo}
+                    year='2024'
+                    country='Portugal'
+                    city='Castelo Branco'
+                    circuit='Kartódromo de Castelo Branco'
+                    class_="EV"
+                    overall="7"
+                    results={{
+                        "Business Plan Presentation": "6", 
+                        "Engineering Design": "9", 
+                        "Cost of Manufacturing": "2"
                     }}
                 />
             </div>
