5abf21ee37208f969c09feae163948ecec71cbc3
Duarte Leitão <duarte0903@gmail.com>
2025-02-12 00:25:16 +0000

    error in img sources

--- src/pages/recruitment/recruitment.jsx
+++ src/pages/recruitment/recruitment.jsx
@@ -46,7 +46,7 @@ const Recruitment = () => {
             <div className='options-container'>
                 <div className='sector-form-container'>
                     <h3>{t('recruitment.mechanical')}</h3>
-                    <img src="/team/mnt.png" className='sector-icon' />
+                    <img src="/system.png" className='sector-icon' />
                     <p className='option-text'>{t('recruitment.mechanical-text')}</p>
                     <a href='https://docs.google.com/forms/d/e/1FAIpQLSeLkUS7CGCfpZke5zjdW2hqaAjIqXxHhIHu-Jy3csdv1jXJTA/viewform?usp=dialog' className='apply-button'>{t('recruitment.apply')}</a>
                 </div>
@@ -60,7 +60,7 @@ const Recruitment = () => {
 
                 <div className='sector-form-container'>
                     <h3>{t('recruitment.management')}</h3>
-                    <img src="/system.png" className='sector-icon' />
+                    <img src="/team/mnt.png" className='sector-icon' />
                     <p className='option-text'>{t('recruitment.management-text')}</p>
                     <a href='https://docs.google.com/forms/d/e/1FAIpQLSdbR2nxJS2SdIE1-lCVs5nK3c_1t7BaDecbpT1JWfwQgOKS_g/viewform?usp=dialog' className='apply-button'>{t('recruitment.apply')}</a>
                 </div>
