057ed25f61fcb1a408322ef7bbef304752aee2a6
Duarte Leitão <duarte0903@gmail.com>
2025-04-09 18:17:14 +0100

    team page updated

--- src/pages/team/team.jsx
+++ src/pages/team/team.jsx
@@ -1,5 +1,5 @@
 import './team.css';
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { useTranslation } from 'react-i18next';
 
 const Team = () => {
@@ -7,57 +7,108 @@ const Team = () => {
 
     const sectors = [
         {
+            id: 'management',
             title: 'Management',
             icon: '/team/mnt.png',
             description: t('team.management'),
+            photo: '/team/mnt_photo.png',
         },
         {
+            id: 'powertrain',
             title: 'Powertrain',
             icon: '/team/pwrt.png',
             description: t('team.powertrain'),
+            photo: '/team/powertrain_photo.png',
         },
         {
+            id: 'esw',
             title: 'Electronics & Software',
             icon: '/team/ecu.png',
             description: t('team.esw'),
+            photo: '/team/esw_photo.png',
         },
         {
+            id: 'drivetrain',
             title: 'Drivetrain',
             icon: '/team/dvrt.png',
             description: t('team.drivetrain'),
+            photo: '/team/drivetrain_photo.png',
         },
         {
+            id: 'chassisaero',
             title: 'Chassis & Aero',
             icon: '/team/chassis_aero.png',
             description: t('team.chassiaero'),
+            photo: '/team/chassis_photo.png',
         },
         {
+            id: 'suspension',
             title: 'Suspension & Steering',
             icon: '/team/suspension_steering.png',
             description: t('team.suspension'),
+            photo: '/team/suspension_photo.png',
         },
     ];
 
+    const [selectedSectorId, setSelectedSectorId] = useState(sectors[0].id);
+    const [isTransitioning, setIsTransitioning] = useState(false);
+    const [displayedSector, setDisplayedSector] = useState(sectors[0]);
+
+    useEffect(() => {
+        if (selectedSectorId) {
+            setIsTransitioning(true);
+            
+            const timer = setTimeout(() => {
+                setDisplayedSector(sectors.find(sector => sector.id === selectedSectorId));
+                setIsTransitioning(false);
+            }, 200);
+            
+            return () => clearTimeout(timer);
+        }
+    }, [selectedSectorId]);
+
     return (
         <div>
-            <h1 data-aos="fade">{t('team.title')}</h1>
+            <div className='team-title-container'
+                 style={{
+                    backgroundImage: `linear-gradient(
+                        rgba(0, 0, 0, 0.5),
+                        rgba(0, 0, 0, 0.5)
+                    ), url('/team/team_photo.png')`,
+                }}>
+                <h1 className='team-title' data-aos="fade">{t('team.title')}</h1>
+            </div>
 
             <p className='team-description' data-aos="fade">{t('team.description')}</p>
 
-            <h1 data-aos="fade">{t('team.sectors-title')}</h1>
+            <div className='sectors-container' data-aos="fade">
+                <h1>{t('team.sectors-title')}</h1>
 
-            <div className='sectors-container'>
-                {sectors.map((sector, index) => (
-                    <div className='sector' data-aos="fade" key={index}>
-                        <div className="sector-content">
-                            <div className="sector-background">
-                                <span className="sector-title">{sector.title}</span>
-                                <img className="sector-icon" src={sector.icon} alt={sector.title} />
-                            </div>
-                            <p className="sector-text">{sector.description}</p>
-                        </div>
+                <div className='sector-selector-container'>
+                    {sectors.map((sector, index) => (
+                        <button 
+                            className={`sector-selector ${selectedSectorId === sector.id ? 'selected' : ''}`} 
+                            key={index} 
+                            type='button'
+                            onClick={() => setSelectedSectorId(sector.id)}
+                        >
+                            {sector.title}
+                        </button>
+                    ))}
+                </div>
+                
+                <div className={`sector-description ${isTransitioning ? 'fade-out' : 'fade-in'}`}>
+                    <div className="sector-content">
+                        <h3 className='sector-title'>{displayedSector.title}</h3>
+                        <p className='sector-description-text'>{displayedSector.description}</p>
                     </div>
-                ))}
+                    
+                    <img 
+                        src={displayedSector.photo}
+                        alt={displayedSector.title} 
+                        className='sector-photo' 
+                    />
+                </div>
             </div>
         </div>
     );
