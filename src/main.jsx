4352f46444c46e7ba3f6980fbdd828aff59c3e4e
Duarte Leitão <duarte0903@gmail.com>
2025-09-04 17:19:44 +0100

    404.html update

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,27 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, useNavigate } from 'react-router-dom';
 import App from './App.jsx';
 import './i18n_config.js';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+function RedirectHandler() {
+  const navigate = useNavigate();
+  useEffect(() => {
+    const params = new URLSearchParams(window.location.search);
+    const redirect = params.get('redirect');
+    if (redirect) {
+      navigate(redirect, { replace: true });
+    }
+  }, [navigate]);
+  return null;
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
   <React.StrictMode>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-  </React.StrictMode>,
+    <BrowserRouter>
+      <RedirectHandler />
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
 );
