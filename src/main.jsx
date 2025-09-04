711fe943b853e9c9e5c9ba44c8f9babcffe3c023
Duarte Leitão <duarte0903@gmail.com>
2025-09-04 17:44:08 +0100

    changes in redirect logic

--- src/main.jsx
+++ src/main.jsx
@@ -1,26 +1,13 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter, useNavigate } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App.jsx';
 import './i18n_config.js';
-
-function RedirectHandler() {
-  const navigate = useNavigate();
-  useEffect(() => {
-    const params = new URLSearchParams(window.location.search);
-    const redirect = params.get('redirect');
-    if (redirect) {
-      navigate(redirect, { replace: true });
-    }
-  }, [navigate]);
-  return null;
-}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <RedirectHandler />
       <App />
     </BrowserRouter>
   </React.StrictMode>
